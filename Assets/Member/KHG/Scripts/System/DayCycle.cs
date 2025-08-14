419bffe2ff08ebec6025ee9f3d393533c421a25b
고한결 <gyeol0411@naver.com>
2025-08-15 01:41:34 +0900

    .update DayCycle

--- Assets/Member/KHG/Scripts/System/DayCycle.cs
+++ Assets/Member/KHG/Scripts/System/DayCycle.cs
@@ -1,8 +1,9 @@
 using UnityEngine;
 using Utility.Unity.Common;
 
-public class DayCycle : MonoBehaviour //시간체크할떄 시작시간 뺴야한다
+public class DayCycle : MonoBehaviour
 {
+    [SerializeField] private float hoursPerSecond = 30;
     [SerializeField] private Transform sun;
     public int _day { get; private set; }
     public int _hour { get; private set; }
@@ -12,27 +13,38 @@ public class DayCycle : MonoBehaviour //시간체크할떄 시작시간 뺴야한다
 
     private void OnEnable()
     {
-        Initialize(0,13);
+        Initialize(0, 13);
     }
 
-    public void Initialize(int d,int h)
+    public void Initialize(int d, int h)
     {
         _day = d;
         _hour = h;
         _totalTime = h + d * 24;
-
-        _timeSet = new DelayInvoker<int>(RaiseTime, 2, 60); //시간 단위 바꾸고싶으면 중간에있는거 수정 ㄱ
+        _timeSet = new DelayInvoker<int>(RaiseTime, 1, hoursPerSecond);
     }
-    private void RaiseTime(int value) => _totalTime += value;
+
+    private void RaiseTime(int value)
+    {
+        _totalTime += value;
+        _day = _totalTime / 24;
+        _hour = _totalTime % 24;
+    }
 
     private void Update()
     {
         _timeSet.Tick();
-        SetSky();
+        SetSky(hoursPerSecond);
     }
 
-    private void SetSky()
+    private void SetSky(float duration)
     {
-        //sun.rotation
+        float currentAngle = sun.rotation.x;
+        float targetAngle = (_hour * 15f) - 90;
+
+        float newAngle = Interpolater.Lerp(currentAngle, targetAngle, duration);
+        sun.rotation = Quaternion.Euler(targetAngle, sun.rotation.y, sun.rotation.z);
+        print(newAngle);
     }
+
 }
