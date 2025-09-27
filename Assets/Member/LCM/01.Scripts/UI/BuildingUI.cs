8e3b3ca6d5864d90a8bf4721e188336b1742013b
SUSOT <a01076754042@gmail.com>
2025-09-28 01:50:33 +0900

    Feat : BuildingIconUI

--- Assets/Member/LCM/01.Scripts/UI/BuildingUI.cs
+++ Assets/Member/LCM/01.Scripts/UI/BuildingUI.cs
@@ -7,26 +7,39 @@ namespace Member.LCM._01.Scripts.UI
     {
         [SerializeField] private RectTransform buttonTrm;
         [SerializeField] private RectTransform buildingPanelTrm;
-
-        [SerializeField] private float upDonwTime = 0.6f;
+        
+        [SerializeField] private float upDownTime = 0.6f;
+        
+        [SerializeField] private BuildingIconUI buildingIconUI;
+        [SerializeField] private Transform contentTrm;
+        
+        [SerializeField] private ObjectDataSO objectData;
         
         private bool _isShow;
-        
+
+        private void Start()
+        {
+            foreach (var building in objectData.objectData)
+            {
+                Instantiate(buildingIconUI, contentTrm).Initialize(building.thisImage, building.Name);
+            }
+        }
+
         public void ClickShowButton()
         {
             if (_isShow)
             {
-                buttonTrm.DORotate(Vector3.zero, upDonwTime, RotateMode.Fast);
+                buttonTrm.DORotate(Vector3.zero, upDownTime, RotateMode.Fast);
                 
-                buttonTrm.DOAnchorPosY(-490f, upDonwTime);
-                buildingPanelTrm.DOAnchorPosY(-650f, upDonwTime);
+                buttonTrm.DOAnchorPosY(-490f, upDownTime);
+                buildingPanelTrm.DOAnchorPosY(-650f, upDownTime);
             }
             else
             {
-                buttonTrm.DORotate(new Vector3(0,0,-180f), upDonwTime, RotateMode.Fast);
+                buttonTrm.DORotate(new Vector3(0,0,-180f), upDownTime, RotateMode.Fast);
                 
-                buttonTrm.DOAnchorPosY(-270f, upDonwTime);
-                buildingPanelTrm.DOAnchorPosY(-430f, upDonwTime);
+                buttonTrm.DOAnchorPosY(-270f, upDownTime);
+                buildingPanelTrm.DOAnchorPosY(-430f, upDownTime);
             }
             _isShow = !_isShow;
         }
