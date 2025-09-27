de9e3d6d7ca80769ead6b0fb78598a9b561cf06e
고한결 <gyeol0411@naver.com>
2025-09-27 12:40:17 +0900

    .update Weather

--- Assets/Member/KHG/Scripts/System/Manager/WeatherManager.cs
+++ Assets/Member/KHG/Scripts/System/Manager/WeatherManager.cs
@@ -1,17 +1,41 @@
 using Core.Events;
+using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public enum Weather
 {
     Clean,
     Cloudy,
-    Rain
+    Rain,
+    NULL
 }
 
 public class WeatherManager : MonoBehaviour
 {
     [SerializeField] private GameEventChannelSO mapChannel;
 
+    private void OnEnable()
+    {
+        StartWeather();
+    }
+
+    private void StartWeather()
+    {
+        StartCoroutine(WeatherCycle());
+    }
+    private IEnumerator WeatherCycle()
+    {
+        yield return new WaitForSeconds(Random.Range(30,180));
+        ChangeWeather(RandomWeather());
+        yield return new WaitForSeconds(Random.Range(30,120));
+        ChangeWeather(Weather.Clean);
+    }
+    private Weather RandomWeather()
+    {
+        int index = Random.Range(0, (int)Weather.NULL - 1);
+        return (Weather)index;
+    }
     private void ChangeWeather(Weather weather)
     {
         var evt = MapEvents.WeatherChangeEvent;
