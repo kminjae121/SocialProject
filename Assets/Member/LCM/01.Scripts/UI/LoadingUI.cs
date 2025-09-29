6e385bf4199d342e75946db4f32be48c451337b7
SUSOT <a01076754042@gmail.com>
2025-09-29 23:23:06 +0900

    Feat : Loading UI

--- Assets/Member/LCM/01.Scripts/UI/LoadingUI.cs
+++ Assets/Member/LCM/01.Scripts/UI/LoadingUI.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
@@ -7,7 +8,18 @@ namespace Member.LCM._01.Scripts.UI
     public class LoadingUI : MonoBehaviour
     {
         [SerializeField] private Image loadingImage;
-        
+        private Camera _camera;
+
+        private void Awake()
+        {
+            _camera = Camera.main;
+        }
+
+        public void SetPosition(Vector3 position)
+        {
+            transform.position = _camera.WorldToScreenPoint(position);
+            transform.rotation = _camera.transform.rotation;
+        }
         public void SetTimeAndStartLoading(float time)
         {
             loadingImage.DOFillAmount(1f, time);
